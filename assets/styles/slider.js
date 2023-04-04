7c1d226e5cbcaef526fc318eda8fc6f036e92de9
EkaterinaPashina <k377at@mail.ru>
2023-04-04 23:44:22 +0500

    Слайдер, без расположения кнопок

---
+++ assets/styles/slider.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 90,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
